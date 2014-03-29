ff847791d0fe3d9dc903f85aaf4e99cda8725d01
Yigit Sadic <yigitsadic@gmail.com>
2014-03-29 21:56:10 +0200

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+  
+  validates :title, :content, :user_id, :category_id, presence: true
+  
+  def get_published_posts
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329162956) do
+ActiveRecord::Schema.define(version: 20140329184433) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140329162956) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.integer  "user_id"
+    t.boolean  "published",   default: false
   end
 
   create_table "users", force: true do |t|
