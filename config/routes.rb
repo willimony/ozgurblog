2690d8a86144388e647314b920754596745a848e
Yigit Sadic <yigitsadic@gmail.com>
2014-03-31 22:35:33 +0300

    la

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Ozgurblog::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  get "/about"       => 'main#about', as: 'about'
-  get "/contact"     => 'main#contact', as: 'contact'
-  get 'search' => 'posts#search', as: 'search'
+#  get "/about"       => 'main#about', as: 'about'
+#  get "/contact"     => 'main#contact', as: 'contact'
+#  get 'search' => 'posts#search', as: 'search'
     
-  scope '/users' do
-    get '/login' => 'sessions#new', as: 'login'
-    get '/logout' => 'sessions#destroy', as: 'logout'
+#  scope '/users' do
+#    get '/login' => 'sessions#new', as: 'login'
+#    get '/logout' => 'sessions#destroy', as: 'logout'
     
-    get '/sign_up' => 'users#new', as: 'sign_up'
-  end
+#    get '/sign_up' => 'users#new', as: 'sign_up'
+#  end
   
-  resources :categories, :users, :sessions, :posts
+  resources :categories, :posts#, :users, :sessions 
   
   root 'posts#index'
 end
