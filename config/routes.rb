a2dd6ae763ef9e3129a114efe81f9baabd850293
Yigit Sadic <yigitsadic@gmail.com>
2014-04-01 22:52:08 +0300

    bootstrap ilerleme

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Ozgurblog::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  get '/about' => 'main#about', as: 'about'
+  get '/contact' => 'main#contact', as: 'contact'
   
   get 'search' => 'posts#search', as: 'search'
   get '/author/:id' => 'main#author', as: 'author'
