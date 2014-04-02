051abf8eb2b3daebf1e008119117525dabc06625
Yigit Sadic <yigitsadic@gmail.com>
2014-04-02 06:15:38 +0300

    admin interface

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -27,4 +27,17 @@ ActiveAdmin.register AdminUser do
     f.actions
   end
 
+  controller do
+    def update_resource object, attributes
+      attributes.each do |attr|
+        if attr[:password].blank? and attr[:password_confirmation].blank?
+          attr.delete :password
+          attr.delete :password_confirmation
+        end
+      end
+
+      object.send :update_attributes, *attributes
+    end
+  end
+  
 end
