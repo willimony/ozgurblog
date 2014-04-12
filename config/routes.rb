64b403d73234b24d760e7401f34f2b055e035805
Yigit Sadic <yigitsadic@gmail.com>
2014-04-12 14:24:52 +0300

    fix markdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Ozgurblog::Application.routes.draw do
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  get '/about' => 'main#about', as: 'about'
   get '/author/:id' => 'admin_users#show', as: 'admin_user'
+  
   get 'search' => 'posts#search', as: 'search'
   get '/feeds' => 'posts#feed', as: 'feeds'
   
   resources :categories, only: [:index, :show]
-  resources :posts, only: [:index, :show]
-  resources :messages, only: [:new, :create]
+  resources :posts,      only: [:index, :show]
+  resources :messages,   only: [:new, :create]
   
   root 'posts#index'
 end
