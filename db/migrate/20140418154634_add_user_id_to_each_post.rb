8a3c0713f0abaef19e38515d9482f4cfa7876276
Yigit Sadic <yigitsadic@gmail.com>
2014-04-19 00:33:12 +0300

    my mistake

--- db/migrate/20140418154634_add_user_id_to_each_post.rb
+++ db/migrate/20140418154634_add_user_id_to_each_post.rb
@@ -1,7 +1,7 @@
 class AddUserIdToEachPost < ActiveRecord::Migration
   def up
   	Post.all.each do |post|
-  		admin_user = post.admin_user
+  		admin_user = AdminUser.find(post.admin_user_id)
   		user = User.find_by(email: admin_user.email)
 
   		post.user = user
